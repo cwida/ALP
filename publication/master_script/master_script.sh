e29e5556149aa10dec3cbc6a8e873d7203d7a7b8
azim <afroozeh3@gmail.com>
2024-11-05 18:35:50 +0100

    fix the master_script for m1

--- publication/master_script/master_script.sh
+++ publication/master_script/master_script.sh
@@ -1,58 +1,68 @@
 #!/bin/bash
 
-# check BENCHMARKING.md for more details.
+# Redirect all command output to master_script.log
+exec >master_script.log 2>&1
 
-WORKSPACE=$(pwd)  # Assuming this is the workspace directory
+green_echo() {
+  # Print to console only
+  echo -e "\033[32m-- $1\033[0m" >/dev/tty
+}
+
+green_echo "Setting up workspace variables..."
+WORKSPACE=$(pwd) # Assuming this is the workspace directory
 REPO_URL="https://github.com/cwida/ALP.git"
-TARGET_DIR="$WORKSPACE/ALP"  # Define target directory for the clone
-BRANCH="main"  # Branch to clone
+TARGET_DIR="$WORKSPACE/ALP" # Define target directory for the clone
+BRANCH="repro"    # Branch to clone
 
+green_echo "Cloning or updating repository..."
 # Clone the repository if it doesn't already exist
 if [ -d "$TARGET_DIR" ]; then
-  echo "Repository already exists, pulling the latest changes from branch $BRANCH..."
+  green_echo "Repository already exists, pulling the latest changes from branch $BRANCH..."
   cd "$TARGET_DIR" && git pull origin "$BRANCH"
 else
-  echo "Cloning the repository and checking out branch $BRANCH..."
+  green_echo "Cloning the repository and checking out branch $BRANCH..."
   git clone --branch "$BRANCH" "$REPO_URL" "$TARGET_DIR"
 fi
 
 # Move to the cloned repository
-# shellcheck disable=SC2164
+green_echo "Navigating to target directory..."
 cd "$TARGET_DIR"
 
 # Create build directory
+green_echo "Creating build directory..."
 mkdir -p "$TARGET_DIR/build"
 
-# Configure CMake
-cmake -DALP_BUILD_PUBLICATION=ON -DCMAKE_TOOLCHAIN_FILE="$TARGET_DIR/toolchain/example.cmake" -S "$TARGET_DIR" -B "$TARGET_DIR/build" -DCMAKE_BUILD_TYPE=Release -DCXX=clang++
+green_echo "Checking if ALP_DATASET_DIR_PATH is set..."
+# Check if ALP_DATASET_DIR_PATH is set
+if [ -z "$ALP_DATASET_DIR_PATH" ]; then
+  echo -e "\033[33mPlease download the dataset from: https://drive.google.com/drive/folders/167faTwZJjqJMKM9Yc6E7KF5LUbsitxJS?usp=sharing\033[0m" >&2
+  echo -e "\033[33mWarning: ALP_DATASET_DIR_PATH is not set!\033[0m" >&2
+  exit 1
+else
+  green_echo "ALP_DATASET_DIR_PATH is set to $ALP_DATASET_DIR_PATH"
+fi
 
+green_echo "Determining toolchain file based on system architecture..."
+# Determine toolchain file based on system architecture
+ARCH=$(uname -m)
+if [ "$ARCH" == "arm64" ]; then
+  TOOLCHAIN_FILE="$TARGET_DIR/toolchain/m1.cmake"
+  green_echo "Using ARM64 toolchain file..."
+else
+  TOOLCHAIN_FILE="$TARGET_DIR/toolchain/x86.cmake"
+  green_echo "Using x86 toolchain file..."
+fi
+
+green_echo "Configuring CMake..."
+# Configure CMake with the selected toolchain file
+cmake -DALP_BUILD_PUBLICATION=ON -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -S "$TARGET_DIR" -B "$TARGET_DIR/build" -DCMAKE_BUILD_TYPE=Release -DCXX=clang++
+
+green_echo "Building the project..."
 # Build the project
 cmake --build "$TARGET_DIR/build" -j 16
 
-# Run tests
-cd "$TARGET_DIR/build" && ctest -j 4
-
-# Check if ALP_DATASET_DIR_PATH is set
-# Set the environment variable `ALP_DATASET_DIR_PATH` with the path to the directory in which the complete
-# binary datasets are located
-if [ -z "$ALP_DATASET_DIR_PATH" ]; then
-  echo -e "\033[33mPlease download the dataset from: https://drive.google.com/drive/folders/167faTwZJjqJMKM9Yc6E7KF5LUbsitxJS?usp=sharing\033[0m"
-  echo -e "\033[33mWarning: ALP_DATASET_DIR_PATH is not set!\033[0m"
-  exit 1
-else
-  echo "ALP_DATASET_DIR_PATH is set to $ALP_DATASET_DIR_PATH"
-fi
-
-# Run benchmarks
-
-# arm64 arch
-"$TARGET_DIR/build/publication/source_code/generated/arm64v8/neon_intrinsic_uf1/arm64v8_neon_intrinsic_1024_uf1_falp_bench"
-
-# x86 arch
-"$TARGET_DIR/build/publication/source_code/generated/x86_64/avx2_intrinsic_uf1/x86_64_avx2_intrinsic_1024_uf1_falp_bench"
-"$TARGET_DIR/build/publication/source_code/generated/x86_64/avx512bw_intrinsic_uf1/x86_64_avx512bw_intrinsic_1024_uf1_falp_bench"
-
-# rest
+green_echo "Running general benchmarks..."
+# Run general benchmarks
 "$TARGET_DIR/build/publication/source_code/bench_compression_ratio/publication_bench_alp_compression_ratio"
 "$TARGET_DIR/build/publication/source_code/bench_compression_ratio/publication_bench_alp32_compression_ratio"
 "$TARGET_DIR/build/publication/source_code/bench_compression_ratio/publication_bench_zstd_compression_ratio"
@@ -70,44 +80,70 @@ fi
 "$TARGET_DIR/build/publication/source_code/bench_speed/publication_bench_patas"
 "$TARGET_DIR/build/publication/source_code/bench_speed/publication_bench_zstd"
 
-# end to end
-"$TARGET_DIR/build/publication/source_code/bench_end_to_end/run_alp"
+green_echo "Running benchmarks based on system architecture..."
+# Run benchmarks based on system architecture
+if [ "$ARCH" == "arm64" ]; then
+  green_echo "Running ARM64 benchmarks..."
+  "$TARGET_DIR/build/publication/source_code/generated/arm64v8/neon_intrinsic_uf1/arm64v8_neon_intrinsic_1024_uf1_falp_bench"
 
-# Set up Python and install dependencies
-python -m pip install --upgrade pip
-pip install -r "$TARGET_DIR/publication/plotter/requirements.txt"
-
-# Run the plotter script
-python "$TARGET_DIR/publication/plotter/plotter.py"
-
-# Clone the new repository
-NEW_REPO_URL="https://github.com/azimafroozeh/bench_ped.git"  # New repository URL
-NEW_TARGET_DIR="$WORKSPACE/BENCH_PED"  # Define new target directory for the clone
-
-if [ -d "$NEW_TARGET_DIR" ]; then
-  echo "New repository already exists, pulling the latest changes from branch $BRANCH..."
-  cd "$NEW_TARGET_DIR" && git pull origin "$BRANCH"
 else
-  echo "Cloning the new repository and checking out branch $BRANCH..."
-  git clone --branch "$BRANCH" "$NEW_REPO_URL" "$NEW_TARGET_DIR"
+  green_echo "Running x86 benchmarks..."
+  "$TARGET_DIR/build/publication/source_code/generated/x86_64/avx2_intrinsic_uf1/x86_64_avx2_intrinsic_1024_uf1_falp_bench"
+  "$TARGET_DIR/build/publication/source_code/generated/x86_64/avx512bw_intrinsic_uf1/x86_64_avx512bw_intrinsic_1024_uf1_falp_bench"
+
+  green_echo "Running end-to-end benchmarks..."
+  # end to end
+  "$TARGET_DIR/build/publication/source_code/bench_end_to_end/run_alp"
+
+  green_echo "Cloning the new repository..."
+  # Clone the new repository
+  NEW_REPO_URL="https://github.com/azimafroozeh/bench_ped.git" # New repository URL
+  NEW_TARGET_DIR="$WORKSPACE/BENCH_PED"                        # Define new target directory for the clone
+
+  if [ -d "$NEW_TARGET_DIR" ]; then
+    green_echo "New repository already exists, pulling the latest changes from branch $BRANCH..."
+    cd "$NEW_TARGET_DIR" && git pull origin "$BRANCH"
+  else
+    green_echo "Cloning the new repository and checking out branch $BRANCH..."
+    git clone --branch "$BRANCH" "$NEW_REPO_URL" "$NEW_TARGET_DIR"
+  fi
+
+  green_echo "Navigating to new target directory..."
+  # shellcheck disable=SC2164
+  cd "$NEW_TARGET_DIR"
+
+  green_echo "Creating build directory for the new repository..."
+  # Create build directory
+  mkdir -p "$NEW_TARGET_DIR/build"
+
+  green_echo "Configuring CMake for the new repository..."
+  # Configure CMake for the new repository
+  cmake -DCMAKE_TOOLCHAIN_FILE="$TOOLCHAIN_FILE" -S "$NEW_TARGET_DIR" -B "$NEW_TARGET_DIR/build" -DCMAKE_BUILD_TYPE=Release -DCXX=clang++
+
+  green_echo "Building the project for the new repository..."
+  # Build the project for the new repository
+  cmake --build "$NEW_TARGET_DIR/build" -j 16
+
+  green_echo "Running specific targets for the new repository..."
+  # Run specific targets
+  cmake --build "$NEW_TARGET_DIR/build" --target bench_ped -j 16
+  cmake --build "$NEW_TARGET_DIR/build" --target test_ped -j 16
+
+  green_echo "Executing the new targets..."
+  # Execute the new targets
+  "$NEW_TARGET_DIR/build/bench_ped"
+  "$NEW_TARGET_DIR/build/test_ped"
 fi
 
-# Move to the new cloned repository
-cd "$NEW_TARGET_DIR"
+green_echo "Setting up Python and installing dependencies..."
+# Set up Python and install dependencies
+{
+  python -m pip install --upgrade pip
+  pip install -r "$TARGET_DIR/publication/plotter/requirements.txt"
+}
 
-# Create build directory
-mkdir -p "$NEW_TARGET_DIR/build"
+green_echo "Running the plotter script..."
+# Run the plotter script
+python3 "$TARGET_DIR/publication/plotter/plotter.py"
 
-# Configure CMake for the new repository
-cmake -DCMAKE_TOOLCHAIN_FILE="$NEW_TARGET_DIR/toolchain/example.cmake" -S "$NEW_TARGET_DIR" -B "$NEW_TARGET_DIR/build" -DCMAKE_BUILD_TYPE=Release -DCXX=clang++
-
-# Build the project for the new repository
-cmake --build "$NEW_TARGET_DIR/build" -j 16
-
-# Run specific targets
-cmake --build "$NEW_TARGET_DIR/build" --target bench_ped -j 16
-cmake --build "$NEW_TARGET_DIR/build" --target test_ped -j 16
-
-# Execute the new targets
-"$NEW_TARGET_DIR/build/bench_ped"
-"$NEW_TARGET_DIR/build/test_ped"
+green_echo "Script execution complete. All logs saved to master_script.log."
