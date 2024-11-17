a3ed8620a3d3c0bc13f4c695022af85c9c11882b
azim <afroozeh3@gmail.com>
2024-11-18 00:27:38 +0100

    [wip] add figure 6

--- publication/plotter/plotter.py
+++ publication/plotter/plotter.py
@@ -1,11 +1,9 @@
-from plot_maker import PlotMaker
-import warnings
-warnings.filterwarnings("ignore", message="findfont: Font family 'Droid Serif' not found")
-        
-if __name__ == "__main__":
-    pm = PlotMaker()
-    pm.plot_architectures() # Figure 4
-    pm.plot_fused_unfused() # Figure 5
-    pm.plot_speed() # Figure 1 
+# from plot_maker import PlotMaker
+from figure_6 import *
 
-    # TODO: Figure 6
+if __name__ == "__main__":
+    # pm = PlotMaker()
+    # pm.plot_architectures()  # Figure 4
+    # pm.plot_fused_unfused()  # Figure 5
+    # pm.plot_speed()  # Figure 1
+    plot_figure_6()
